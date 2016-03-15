333ba751681fb0e833d9338ac8ab9ece8d4672c5
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-03-15 16:42:11 +0100

    importing last public gists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
+    @gists = []
+    api_url = 'https://api.github.com/gists'
+    open(api_url) do |stream|
+      @gists << JSON.parse(stream.read)
+    end
+    @gists.flatten!
   end
 
 
