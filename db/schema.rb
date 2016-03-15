91476b67ef0ff1d41d9f3f3cb27b944d6a51acdf
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-03-15 14:17:36 +0100

    adding github username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315123228) do
+ActiveRecord::Schema.define(version: 20160315131707) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20160315123228) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
+    t.string   "username",               limit: 255
+    t.string   "token",                  limit: 255
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
