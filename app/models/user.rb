1ab9cf5675b84ca0080fa78be3b1f9efae5a7890
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-03-15 15:47:37 +0100

    setting user model in order to get all necessary info from Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:github]
+
+
+
+  def self.find_for_github_oauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.picture = auth.info.image
+        user.password = Devise.friendly_token[0,20]  # Fake password for validation
+        user.username = auth.info.nickname
+        user.name = auth.info.name
+        user.github = auth.info.urls.GitHub
+        user.token = auth.credentials.token
+      end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315131707) do
+ActiveRecord::Schema.define(version: 20160315143805) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20160315131707) do
     t.string   "username",               limit: 255
     t.string   "token",                  limit: 255
     t.datetime "token_expiry"
+    t.string   "name",                   limit: 255
+    t.string   "github",                 limit: 255
+    t.string   "picture",                limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
